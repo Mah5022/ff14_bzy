dd7684ffc12906d1c8ebc488aa528fdde10ef755
zxc523893764 <523893764@qq.com>
2021-05-24 15:40:28 +0800

    1.

--- static/rem.js
+++ static/rem.js
@@ -5,7 +5,6 @@
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
             if(clientWidth>=750){
-                // 这里的640 取决于设计稿的宽度
                 docEl.style.fontSize = '100px';
             }else{
                 docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
