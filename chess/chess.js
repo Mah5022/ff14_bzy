6c943cc1ca5b6cd92451ea7e8747019d3bc807dd
Mah5022 <523893764@qq.com>
2021-05-24 21:12:56 +0800

    1.

--- chess/chess.js
+++ chess/chess.js
@@ -1,5 +1,6 @@
 $(function () {
     let init_state = 0;
+    let countdown = 23,timer = null;//倒计时
     let start_spot;//安全点 0:南侧C点 1:北侧A点
     let soldier_top,soldier_left,soldier_right,soldier_bottom;//小怪计步
     let soldier1_start = 0,soldier2_start = 0;//小怪起始点 soldier1:0北左南右 1北右南左  soldier2:0东上西下 1东下西上
@@ -9,6 +10,36 @@ $(function () {
         [2,1],[2,2],[2,3],
         [3,1],[3,2],
     ];
+
+    //初始化
+    function init() {
+        init_state = 0;
+        countdown = 23;
+        clearInterval(timer);
+        timer = null;
+
+        $(".buff>.countdown").text("");
+        $("ul.center>li").removeClass("start die success");
+        $(".buff>i").attr("class","");
+        $(".soldier").removeClass("soldier-top soldier-bottom soldier-left soldier-right num1 num2 num3");
+        $(".popup").show().children(".start").show();
+
+        chess_start();
+        timer = setInterval(function () {
+            $(".buff>.countdown1").text(countdown);
+            $(".buff>.countdown2").text(countdown+15);
+            countdown--;
+            if(countdown<0){
+                clearInterval(timer);
+                timer = null;
+                $(".popup").show().children(".die").show();
+            }
+        },1000);
+        setTimeout(function () {
+            $(".popup").hide().children(".start").hide();
+            console.log(timer);
+        },1000);
+    }
 
     //棋盘生成
     function chess_start() {
@@ -35,17 +66,6 @@ $(function () {
         init_state = 1;
     }
 
-    //初始化
-    function init() {
-        init_state = 0;
-
-        $("ul.center>li").removeClass("start");
-        $(".buff>i").attr("class","");
-        $(".soldier").removeClass("soldier-top soldier-bottom num1 num2 num3");
-
-        chess_start();
-    }
-
     //判断点 左下角[1,1] spot_x:横坐标 spot_y:纵坐标
     function chess_check(spot_x,spot_y){
         if(!init_state)return false;
@@ -62,6 +82,11 @@ $(function () {
         //东西侧小怪判定
         if(die_row.indexOf(spot_y)>=0){
             //死了
+            if(timer){
+                clearInterval(timer);
+                timer = null;
+                $(".popup").show().children(".die").show();
+            }
             $("li[class='"+spot_x+"-"+spot_y+"']").addClass("die");
             return false;
         }
@@ -78,7 +103,13 @@ $(function () {
             let first_y = spot_y-(buff_1-Math.abs(move_x));
             if(first_y>=1&&first_y<=5){
                 if(Math.abs(first_x-3)+Math.abs(first_y-(start_spot?5:1))==buff_2){
-                    console.log("YOU WINER!");
+                    //活了
+                    if(timer){
+                        clearInterval(timer);
+                        timer = null;
+                        $(".popup").show().children(".success").show();
+                    }
+                    $("li[class='"+spot_x+"-"+spot_y+"']").addClass("success");
                     return false;
                 }
             }
@@ -86,13 +117,24 @@ $(function () {
             first_y = spot_y+(buff_1-Math.abs(move_x));
             if(first_y>=1&&first_y<=5){
                 if(Math.abs(first_x-3)+Math.abs(first_y-(start_spot?5:1))==buff_2){
-                    console.log("YOU WINER!");
+                    //活了
+                    if(timer){
+                        clearInterval(timer);
+                        timer = null;
+                        $(".popup").show().children(".success").show();
+                    }
+                    $("li[class='"+spot_x+"-"+spot_y+"']").addClass("success");
                     return false;
                 }
             }
         }
 
         //死了
+        if(timer){
+            clearInterval(timer);
+            timer = null;
+            $(".popup").show().children(".die").show();
+        }
         $("li[class='"+spot_x+"-"+spot_y+"']").addClass("die");
         return false;
     }
@@ -102,5 +144,16 @@ $(function () {
         chess_check(Number($(this).data("x")),Number($(this).data("y")));
     })
 
-    init();
+    //开始
+    $(".btn>a.start").click(function () {init();});
+
+    $(".btn>a.help").click(function () {
+        $(".popup").show().children(".help").show();
+    });
+
+    $(".popup>a.close").click(function () {
+        $(".popup").hide().children(".text").hide();
+    })
+
+    $(".popup").show().children(".help").show();
 })
